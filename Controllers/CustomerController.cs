b2e5164cd4683ea2a9c4ebd9648692fa050365d5
angelo burias <angeloburias8@gmail.com>
2021-08-02 00:37:20 +0800

    Add project files.

---
+++ Controllers/CustomerController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Th_E_shop.Views.Customer
+{
+    public class CustomerController : Controller
+    {
+
+        
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Th_E_shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
